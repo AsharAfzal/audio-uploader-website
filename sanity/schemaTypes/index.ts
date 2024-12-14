305780decfe40c026c38120f867b1ba482889484
Ashar Afzal <160453301+AsharAfzal@users.noreply.github.com>
2024-12-14 17:58:49 +0500

    Delete sanity directory

--- sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import audioUploader from './audio-uploader'
-
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [audioUploader],
-}
