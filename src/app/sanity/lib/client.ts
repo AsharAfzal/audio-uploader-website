6e9c599c73c5289c9b008ac981f727f81f7efb5f
Ashar Afzal <160453301+AsharAfzal@users.noreply.github.com>
2024-12-21 15:00:58 +0500

    Update client.ts

--- src/app/sanity/lib/client.ts
+++ src/app/sanity/lib/client.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@sanity/client';
 
 const sanityClient = createClient({
-  projectId: 'cnzg51pd', // Replace with your project ID
+  projectId: 'zdhmhor7', // Replace with your project ID
   dataset: 'production', // Replace with your dataset
   apiVersion: '2023-12-14', // Use today's date or the latest version
-  useCdn: true, // Use `false` if you need fresh data
+  useCdn: false, // Use `false` if you need fresh data
 });
 
 export default sanityClient;
